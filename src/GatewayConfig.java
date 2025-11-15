3dd10be87de7910759117c8842642ee86cfbd16c
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-15 15:10:55 +0300

    Update GatewayConfig.java

--- src/GatewayConfig.java
+++ src/GatewayConfig.java
@@ -1,1 +1,23 @@
+package com.psyche.platform.apigateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("user-service", r -> r.path("/api/users/**")
+                .uri("lb://user-service"))
+            .route("prediction-service", r -> r.path("/api/predictions/**")
+                .filters(f -> f.circuitBreaker(c -> c.setName("predictionCB")))
+                .uri("lb://prediction-service"))
+            .route("analytics-service", r -> r.path("/api/analytics/**")
+                .uri("lb://analytics-service"))
+            .build();
+    }
+}
