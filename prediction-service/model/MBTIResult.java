6dde50835ea50140d7e04a792bd157983e199aaf
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-17 11:44:01 +0300

    Update MBTIResult.java

--- prediction-service/model/MBTIResult.java
+++ prediction-service/model/MBTIResult.java
@@ -1,1 +1,25 @@
+package com.psyche.platform.prediction.model;
 
+import lombok.Builder;
+import lombok.Data;
+import java.util.Map;
+
+@Data
+@Builder
+public class MBTIResult
+  {
+    private String mbtiType;
+    private Map<String, Double> traitScores; 
+    private Double confidence;
+    private Double bullyingRisk;
+    private String analysis;
+   
+    public static MBTIResult createHighConfidenceResult(String type, Double confidence) 
+    {
+        return MBTIResult.builder()
+            .mbtiType(type)
+            .confidence(confidence)
+            .bullyingRisk(0.0)
+            .build();
+    }
+}
