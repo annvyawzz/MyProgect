1908be0a32a633423dac363eb4c7b30649470a88
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-17 11:43:05 +0300

    Update  PredictionServiceApplication.java

--- prediction-service/📁 src/📄 PredictionServiceApplication.java
+++ prediction-service/📁 src/📄 PredictionServiceApplication.java
@@ -1,1 +1,17 @@
+package com.psyche.platform.prediction;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCaching
+public class PredictionServiceApplication 
+{
+    public static void main(String[] args)
+  {
+        SpringApplication.run(PredictionServiceApplication.class, args);
+    }
+}
