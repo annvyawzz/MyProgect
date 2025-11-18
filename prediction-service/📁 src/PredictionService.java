5a695b6c71bc2ef3703fad77d2deab0974a70468
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-18 16:23:38 +0300

    Update PredictionService.java

--- prediction-service/📁 src/PredictionService.java
+++ prediction-service/📁 src/PredictionService.java
@@ -1,1 +1,10 @@
+package com.psyche.platform.prediction.command;
 
+import com.psyche.platform.prediction.model.MBTIResult;
+
+// Command Pattern
+public interface PredictionCommand {
+    MBTIResult execute();
+    void undo();
+    String getCommandName();
+}
