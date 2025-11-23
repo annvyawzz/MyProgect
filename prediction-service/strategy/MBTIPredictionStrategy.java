efb6a2650ab127e13a2045e572b1a166dc509a3d
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-23 19:11:35 +0300

    Update MBTIPredictionStrategy.java

--- prediction-service/strategy/MBTIPredictionStrategy.java
+++ prediction-service/strategy/MBTIPredictionStrategy.java
@@ -1,1 +1,12 @@
+package com.psyche.platform.prediction.strategy;
 
+import com.psyche.platform.prediction.model.MBTIResult;
+import com.psyche.platform.prediction.model.ParentsGeneticData;
+import com.psyche.platform.prediction.model.EnvironmentData;
+
+// Strategy Pattern
+public interface MBTIPredictionStrategy 
+{
+    MBTIResult predict(ParentsGeneticData parents, EnvironmentData environment);
+    String getStrategyName();
+}
